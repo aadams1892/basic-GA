48f7fb82a12faca6bbceb7c644ada0074ddcf6bb
Andrew A <72630976+program-terminated@users.noreply.github.com>
2024-05-24 12:57:18 -0400

    Update file

---
+++ model/survivor-selection.py
@@ -0,0 +1,18 @@
+# (μ + λ) survivor selection implementation
+def mu_plus_lambda(offspring, current_pop):
+    total_pop = offspring + current_pop
+
+    # Sort the population by fitness
+    pop_and_fit = fitness(total_pop, False)
+
+    return pop_and_fit[:len(current_pop)]
+    
+# Replacement survivor selection implementation
+def replacement(offspring, current_pop):
+
+    # Sort the population by fitness
+    sorted_pop = fitness(current_pop, False)
+
+    next_gen = offspring + sorted_pop[len(offspring)+1:]
+
+    return next_gen
