0a3b0430b4241c8958b5b0a2de67bc57067a0e94
Andrew A <72630976+program-terminated@users.noreply.github.com>
2024-05-28 13:35:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-# ADD IMPORTS TO ALL FILES AS NEEDED
+from matplotlib import pyplot as mpp
+import genetic-algorithm.py
 
-pop_size = 1000
-indiv_len = 150
-parent_selection = ["roulette", 10]
-crossover_spec = ["uniform", 0.85]
-mutation_spec = ["scramble", [2, 6], 0.8]
+# GA parameters
+pop_size = 10000
+indiv_len = 250
+parent_selection = ["tournament", 100, 100]
+crossover_spec = ["n_point", 15, 0.9]
+mutation_spec = ["scramble", [50, 75], 0.9]
 survivor_selection = ["mu_plus_lambda"]
 optimal_fitness = indiv_len
-max_gen = 250
-verbose = 0 # If you want extended information about each generation
+max_gen = 300
+verbose = 1 # If you want extended information about each generation
 
 g = ga(pop_size, indiv_len, parent_selection, crossover_spec, mutation_spec, survivor_selection, optimal_fitness, max_gen, verbose)
 print("Highest fitness:", g[0])
 print("Generations simulated:", g[1])
 print("Average fitness increase each generation:", g[2])
 print("Starting average fitness:", g[3][0])
-print("Ending average fitness:", g[3][1])
+print("Ending average fitness:", g[3][-1])
+
+# Average fitness per generation
+fig1 = mpp.figure()
+mpp.plot(np.arange(0, g[1]+1, 1), g[3])
+
+# Average fitness increase per generation
+fig2 = mpp.figure()
+mpp.plot(np.arange(1, g[1]+1, 1), g[4])
+
+# Show figures
+mpp.show()
