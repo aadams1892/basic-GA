2cf8f3d7ab49bb4aad20528d297e43b2c4ce1a04
Andrew A <72630976+program-terminated@users.noreply.github.com>
2024-05-28 13:18:31 -0400

    Update population-initialization.py

--- model-functions/population-initialization.py
+++ model-functions/population-initialization.py
@@ -8,8 +8,11 @@ def init(pop_size, indiv_length):
     # Create population of individuals
     for i in range(pop_size):
         for _ in range(indiv_length):
+            # Add either a 0 or 1 to the individual
             indiv.append(random.randint(0,1))
+        # Add the individual to the population
         population.append(indiv)
+        # Reset individual
         indiv = []
 
     return population
@@ -17,3 +20,4 @@ def init(pop_size, indiv_length):
 if DEBUG:
     test_pop = init(10, 10)
     print(test_pop)
+    
